2bf4f6ead78a2814bf9b4f11bb6892ce03280087
Lukáš Stuchlík <stuchlik1@seznam.cz>
2019-04-08 07:32:03 +0200

    Added INSYS

---
+++ insys/precache-manifest.22592b927d375338a24c9446363789fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dac5f487d75b652b2d2c",
+    "url": "/static/js/main.dac5f487.chunk.js"
+  },
+  {
+    "revision": "709338cad565ef94a73c",
+    "url": "/static/js/2.709338ca.chunk.js"
+  },
+  {
+    "revision": "709338cad565ef94a73c",
+    "url": "/static/css/2.af35d720.chunk.css"
+  },
+  {
+    "revision": "752f28ba70fa6431914e104c49a37ba3",
+    "url": "/index.html"
+  }
+];
